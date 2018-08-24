02586dc3c60ea87f3c14f07b3d7e6ad49386be96
hzq <13402318457@163.com>
2018-08-24 17:14:06 +0800

    dist

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -9,7 +9,7 @@
         name: 'HelloWorld',
         data() {
             return {
-                msg: 'Welcome to LiuFan'
+                msg: 'Welcome to LiuFan个人网站'
             };
         }
     };
