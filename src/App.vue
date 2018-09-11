297d39aced087ac18acf24c8ce66b05c9b9ba986
hzq <13402318457@163.com.com>
2018-09-11 23:36:16 +0800

    147852

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <template>
     <div id="app" :style="{'padding-bottom':$route.name==='hand-index'?'0':'0.57rem'}">
         <!-- <keep-alive> -->
-        <router-view class="royter-view" />
+        <router-view />
         <!-- </keep-alive> -->
-        <l-footer></l-footer>
+        <!-- <l-footer></l-footer> -->
     </div>
 </template>
 
