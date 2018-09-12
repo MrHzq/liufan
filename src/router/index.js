c637ffe8929b366c357eabb3172eb5cb5f96b88f
hzq <13402318457@163.com>
2018-09-12 12:39:44 +0800

    asasasasasa

--- src/router/index.js
+++ src/router/index.js
@@ -18,12 +18,12 @@ import Router from 'vue-router';
 Vue.use(Router);
 
 export default new Router({
-    mode: 'history',
-    scrollBehavior(to, from, savedPosition) {
-        if (savedPosition) {
-            return savedPosition;
-        } else return { x: 0, y: 0 };
-    },
+    // mode: 'history',
+    // scrollBehavior(to, from, savedPosition) {
+    //     if (savedPosition) {
+    //         return savedPosition;
+    //     } else return { x: 0, y: 0 };
+    // },
     routes: [
         { path: '/', redirect: '/home' },
         {
